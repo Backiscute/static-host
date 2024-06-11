fb76f1df8df76fccc7ac813a63918734617f158f
Back <back2lobby0iq@gmail.com>
2024-06-11 18:00:15 +0100

    added handler for rest

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,13 @@ if (!process.env.PORT) {
 
 const app = express();
 
+app.get("/", (req, res) => res.send("<h1>Nothing to see here!</h1>"));
 app.use(express.static("public"));
+app.get("*", (req, res) => {
+    const path = req.path.split("/")[1];
+    
+    res.sendFile(`./public/${path}/index.html`, { root: "."});
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is listening on port ${process.env.PORT}`);
